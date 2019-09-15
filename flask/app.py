a463c1224fad503c48c069649dbc91cf3f88c9e6
mudit9 <msgagent1@gmail.com>
2019-09-15 07:43:21 -0400

    flask app

--- flask/app.py
+++ flask/app.py
@@ -7,10 +7,25 @@ inverted_index = InvertedIndex()
 @app.route("/Search", methods=['GET','POST'])
 def index():
     search = str(request.args['search'])
-    print(search)
     query = inverted_index.search(search)
+    links = []
+    frames = []
+    #print(inverted_index.inverted_index['human'])
     print(query)
-    return render_template("index.html")
+    total_frames  = []
+    video_titles= []
+    video_thumbnails = []
+    video_links = []
+    for item in query:
+        pervideoframe = []
+        video_titles.append(query[item]['title'])
+        video_thumbnails.append(query[item]['thumbnail_link'])
+        video_links.append(query[item]['youtube_link'][:-1])
+
+        for frames in query[item]['frames']:
+            pervideoframe.append(frames['frame_no'])
+        total_frames.append(pervideoframe)
+    return render_template("list.html",video_links=video_links,total_frames=total_frames,video_titles=video_titles,video_thumbnails=video_thumbnails,pervideoframe=pervideoframe)
 
 @app.route('/')
 def show_stuff():
